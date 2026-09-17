c6adacd2389d3d254088bff73b3ef55923d8b3d9
Legrand <legrandborisohandjaedimo@gmailcom>
2026-09-17 20:37:36 +0300

    fix: CI bootstrap SQL syntax and enforce ON_ERROR_STOP

--- bootstrap.sql
+++ bootstrap.sql
@@ -3,12 +3,14 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'app_user') THEN
-      CREATE ROLE app_user LOGIN PASSWORD 'app_password' NOSUPERUSER NOCREATEDB NOCREATEROLE NOBYPASSRLS;
-      GRANT EXECUTE ON FUNCTION public.resolve_session(TEXT) TO app_user;
+      WHERE rolname = 'app_user') THEN
+      CREATE ROLE app_user LOGIN PASSWORD 'app_password'
+        NOSUPERUSER NOCREATEDB NOCREATEROLE NOBYPASSRLS;
    END IF;
 END
 $do$;
+
+GRANT EXECUTE ON FUNCTION public.resolve_session(TEXT) TO app_user;
 GRANT CONNECT ON DATABASE luxia_db TO app_user;
 GRANT USAGE ON SCHEMA public TO app_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_user;
